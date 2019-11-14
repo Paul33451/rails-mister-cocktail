58f56efa62765ff70aedeeef39ffa646263f1c56
Paul Manche <paul.manche1@gmail.com>
2019-11-14 17:36:34 +0100

    done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  # def new
+  #   @dose = Dose.new
+  #   @ingredient = Ingredient.new
+  # end
+# def new
+#   @dose = Dose.new
+# end
+
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  # def ingredient_params
+  #   params.require(:ingredient).permit(:name)
+  # end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Pina colada")
+Cocktail.create(name: "Sex on the beach")
