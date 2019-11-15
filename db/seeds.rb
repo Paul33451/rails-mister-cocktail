66ab8c950a2ca695b80e29d634820e3c17c1b7ce
Paul Manche <paul.manche1@gmail.com>
2019-11-15 12:26:45 +0100

    before style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Pina colada")
-Cocktail.create(name: "Sex on the beach")
+#
+Cocktail.all.destroy
+Ingredient.all.destroy
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingrdient_serialized = open(url).read
+ingredients = JSON.parse(ingrdient_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+url2 = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+cocktails_serialized = open(url2).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strCategory"])
+end
